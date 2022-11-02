084ef94b232613ee76df28b57f2b3a94f6e7a3e0
mellefmedali <mellef_med_ali@hotmail.fr>
2022-11-02 15:44:13 +0100

    second commit

---
+++ src/UserList.js
@@ -0,0 +1,11 @@
+
+// OK Create a project using create-react-app.
+// OK Create a project using create-react-app.
+// OK Create UserList.js file in src folder
+// OK Install axios.
+
+// You gone to use jsonplaceholder API to get list of user 
+// Use axios to Get data from Link inside useEffect hooks
+// Use UseState hooks to save data into listOfUSer state 
+// Map listOfUser to display list of user into screen 
+// Style your App as you wish
